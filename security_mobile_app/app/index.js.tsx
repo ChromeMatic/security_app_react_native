06de9792a1e9d34c8ab4253f2ee21b86dabcac6a
Dev <brownar@cokcu.com>
2024-02-21 11:39:28 -0500

    Tailwindcss add to project

--- security_mobile_app/app/index.js.tsx
+++ security_mobile_app/app/index.js.tsx
@@ -2,8 +2,12 @@ import { View, Text } from "react-native";
 
 const Home = () =>{
     return(
-        <View>
-            <Text>Home Page</Text>
+        <View className="flex-1 justify-center items-center bg-green-50">
+            <Text
+                className="font-mono text-2xl text-teal-600"
+            >
+                Home Page
+            </Text>
         </View>
     )
 }
