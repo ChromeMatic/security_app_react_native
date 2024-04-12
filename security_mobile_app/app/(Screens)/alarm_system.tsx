379c0d1d9830b970803e22c5e86a440f0eff4885
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2024-04-11 21:22:02 -0500

    more chnages added

--- security_mobile_app/app/(Screens)/alarm_system.tsx
+++ security_mobile_app/app/(Screens)/alarm_system.tsx
@@ -1,16 +1,42 @@
-import { SafeAreaView, Text} from 'react-native'
+import { SafeAreaView, Text, TouchableOpacity, Alert} from 'react-native'
 import { supabase } from "../../lib/supabase";
 import { useState, useEffect } from 'react';
+import { Database } from "../../types/supabase";
+import FontAwesome from '@expo/vector-icons/FontAwesome6';
 import React from 'react';
 
 const alarm_system = () =>{
 
-    const [User, setUser] = useState({})
+    type _alarm = Database['public']['Tables']['Alarm']['Row']
+    const [User, setUser] = useState("" as string)
 
     async function get_login_user() {
-        const user = await supabase.auth.getUser()
-        setUser(user.data)
-        return User
+        const { data: { user } } = await supabase.auth.getUser()
+        let user_id:string = ''+user?.id
+
+        if(user_id){ setUser(user_id) }
+    }
+
+    async function _Alert(){
+       const { data, error } = await supabase
+       .from('Alarm')
+       .select("*")
+       .eq('user_id',User)
+
+       if(data){
+            const { error} = await supabase
+            .from('Alarm')
+            .insert([
+            { 
+                user_id: ''+User, 
+                alarm_status: true 
+            },
+            ])
+            .select()
+    
+            if(error){ Alert.alert(error.message) }
+       }
+
     }
 
     useEffect(()=>{ get_login_user() },[])
@@ -18,12 +44,19 @@ const alarm_system = () =>{
     return(
         
         <SafeAreaView 
-          className="w-full h-screen flex-1 justify-center 
-          items-center px-4 py-2 bg-main"
+          className="w-full h-screen flex flex-col space-y-8 justify-center items-center px-4 py-2 bg-main"
         >
-            <Text className="text-white text-4xl">
-                Alarm System
-            </Text>
+
+            <TouchableOpacity
+              className="border-2 flex flex-col space-y-2 p-2 w-full border-teal-900 rounded-md
+              justify-center items-center"
+              onPress={_Alert}
+            >
+                <FontAwesome name="bell" size={24} color="#14b8a6" />
+                <Text className="text-teal-500">
+                    Alarm System
+                </Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
